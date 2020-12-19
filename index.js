da3a6eb9e5e2477464c564051542babda4386506
Giuliano Brito <giulianobr@gmail.com>
2020-12-18 22:24:57 -0300

    remove

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ var app = express();
 var cors = require('cors');
 var fs = require('fs');
 var data = fs.readFileSync('./data.json','utf8');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.use(cors());
 
-
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   res.json(data);
 });
